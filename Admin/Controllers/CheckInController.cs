9d63c24d69a24655ec778190abe49828e1b7e5a9
Gilberto Junior <gilbertojunior533@gmail.com>
2018-11-09 10:25:23 -0200

    Adicionado tela de check-in.

---
+++ Admin/Controllers/CheckInController.cs
@@ -0,0 +1,57 @@
+﻿using Admin.Helppser;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+
+namespace Admin.Controllers
+{
+    public class CheckInController : Controller
+    {
+        // GET: CheckIn
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public string GetQrCode()
+        {
+            var usuario = PixCoreValues.UsuarioLogado;
+            var keyUrl = ConfigurationManager.AppSettings["UrlAPI"].ToString();
+            var url = keyUrl + "/Seguranca/WpQrCode/GeraCodigoQr/" + usuario.idCliente + "/" + usuario.IdUsuario;
+
+            object envio = new
+            {
+               usuario.idCliente,
+               idUsuario = usuario.IdUsuario,
+            };
+
+            var jss = new JavaScriptSerializer();
+            var data = jss.Serialize(envio);
+
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+            httpWebRequest.ContentType = "application/json";
+            httpWebRequest.Method = "POST";
+
+            using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+            {
+                streamWriter.Write(data);
+                streamWriter.Flush();
+                streamWriter.Close();
+            }
+
+            var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                var result = streamReader.ReadToEnd();
+                return result;
+            }
+        }
+    }
+}
---
+++ Admin/Scripts/Pages/checkin.js
@@ -0,0 +1,28 @@
+﻿$('#qrCode').hide();
+
+function setQrCodeImage(response) {
+    let result = $.parseJSON(response);
+    $("#loading").hide();
+    $('#qrCode').attr('src', "data:image/png;base64," + result.qrCode);
+    $('#qrCode').show();
+}
+
+function getQrCode() {
+    var Url = "/CheckIn/GetQrCode";
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": Url,
+        "method": "GET"
+    };
+
+    $.ajax(settings).done(function (response) {
+        setQrCodeImage(response);
+    });
+
+    setTimeout(function () {
+        getQrCode();
+    }, 60000);
+}
+
+$(document).ready(getQrCode());
